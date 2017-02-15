b9ca4d64af49cc425fd4f9c6c067f667949af150
Kerstin <kerstin.labuda@gmail.com>
2017-02-15 16:27:19 -0600

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -9,27 +9,10 @@ public class Bank {
 		Accounts.put(accountNum, a);
 	}
 	
-	public void deposit(String accountNum, int amount) throws Exception {
-		if (validate(accountNum)) {
-			Accounts.get(accountNum).deposit(amount);
-		}
-		else
-			throw new Exception("Account does not exist");
-	}
-	
-	public void withdraw(String accountNum, int amount) throws Exception {
-		if (validate(accountNum)) {
-			Accounts.get(accountNum).withdraw(amount);
-		}
-		else
-			throw new Exception("Account does not exist");
-	}
-	
-	public boolean validate(String accountNum) {
+	public Account validate(String accountNum) {
 		if(Accounts.containsKey(accountNum)) {
-			return true;
+			return Accounts.get(accountNum);
 		}
-		
-		return false;
+		return null;
 	}
 }
