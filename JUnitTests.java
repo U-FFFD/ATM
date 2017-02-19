6d382ddbee6cc90a25ba1b7c81ba1700969f1770
Emmett Wesolowski <mmettej@gmail.com>
2017-02-18 21:48:26 -0600

    Update JUnitTests.java
    Added full test sets for bank and atm

--- JUnitTests.java
+++ JUnitTests.java
@@ -1,78 +1,165 @@
 import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class JUnitTests {
-
-	public JUnitTests() {
-		// TODO Auto-generated constructor stub
-	}
 	
-	@BeforeClass
-	public static void beforeClass() {
-		System.out.println("BeforeClass\n Starting CLI.");
-		BSim simulator = new Bsim();
-	}
-	@Test (expected = Exception.class)
-	public static void newAccount() {
-		//try random account number. 
-		Bsim("12345");
-		
-	}
-	@Test
-	public static void startATM() {
-		ATM atm = new ATM();
-	}
-	@Test 
-	public static void validateCard() {
-		
-	}
 	// ACCOUNT TESTS
 	@Test //DONE
-	public static void validateAccount() {
+	public  void validateAccount() {
 		String pin = "5556";
 		Account testAcc = new Account("1111", pin, 500);
 		assertFalse(testAcc.validate("12358"));
 		assertTrue(testAcc.validate("5556"));
 	}
 	@Test //DONE
-	public static void widthdraw() throws Exception {
+	public  void widthdraw() throws Exception {
 		Account testAcc = new Account("4444", "1234", 500);
 		testAcc.withdraw(350);
 		assertTrue(testAcc.getBalance() == 150);
 	}
 	@Test //DONE
-	public static void deposit() {
+	public  void deposit() {
 		Account testAcc = new Account("1111", "1234", 200);
 		testAcc.deposit(250);
 		assertTrue(testAcc.getBalance() == 450);
 	}
 	@Test //DONE
-	public static void widthdrawExceed() {
+	public  void widthdrawExceed() {
 		Account testAcc = new Account("1234", "5555", 60);
 		try {
 			testAcc.withdraw(600);
 		} catch (Exception e) {
+			System.out.println("Withdrawl exceed test, expect err message: ");
 			System.out.println(e.getMessage());
 			assertTrue("Withdrawal amount exceeds balance".equalsIgnoreCase(e.getMessage()));
 		}		
 	}
 	//END ACCOUNT
-	public static void wrongPin(){
-		int pin = 15328;
-		atm.enterPin(pin); 	// call pin method of the ATM.
-		
-
+	
+	// BANK TESTS
+	public  void bankTest() {
+		Bank testBank = new Bank();
+		String pin2 = "4444";
+		testBank.createAccount("5568", "1325", 50);
+		testBank.createAccount("1333", "4444", 20);
+		testBank.createAccount("1324", "1111", 9000);
+		testBank.createAccount("8895", "1956", 200);
+		testBank.createAccount("0001", "7779", 0);
+		Account testAcc = testBank.validate("1333");
+		assertTrue(testAcc.getBalance() == 20);
+		assertTrue(testAcc.validate(pin2));
 	}
-	public static void testSim() {
-		
+	// END BANK
+	
+	// ATM TESTS	
+	@Test
+	public  void validateATM() {
+		Bank testBank = new Bank();
+		testBank.createAccount("5568", "1325", 50);
+		testBank.createAccount("1333", "4444", 20);
+		testBank.createAccount("1324", "1111", 9000);
+		testBank.createAccount("8895", "1956", 200);
+		testBank.createAccount("0001", "7779", 0);
+		ATM atm = new ATM(testBank); //start ATM with bank filled with accounts.
+		assertFalse(atm.cardInserted("1666"));	//invalid account
+		assertTrue(atm.cardInserted("8895"));	//valid account	
+		atm.close();
+	}
+	@Test
+	public  void checkPin() {
+		Bank testBank = new Bank();
+		testBank.createAccount("5568", "1325", 50);
+		testBank.createAccount("1333", "4444", 20);
+		testBank.createAccount("1324", "1111", 9000);
+		testBank.createAccount("8895", "1956", 200);
+		testBank.createAccount("0001", "7779", 0);
+		ATM atm = new ATM(testBank); //start ATM with bank filled with accounts.
+		assertTrue(atm.cardInserted("1324"));
+		assertFalse(atm.checkPin("1355")); //false entry
+		assertFalse(atm.checkPin("79635532")); //very false entry
+		assertTrue(atm.checkPin("1111")); 	//correct pin, should be true.
+		atm.close();
+	}
+	@Test 
+	public  void atmWithdraw() {
+		Bank testBank = new Bank();
+		testBank.createAccount("5568", "1325", 50);
+		testBank.createAccount("1333", "4444", 20);
+		testBank.createAccount("1324", "1111", 9000);
+		testBank.createAccount("8895", "1956", 200);
+		testBank.createAccount("0001", "7779", 0);
+		ATM atm = new ATM(testBank); //start ATM with bank filled with accounts.
+		assertTrue(atm.cardInserted("1324"));
+		atm.checkPin("1111");
+		atm.withdraw(500);
+		assertTrue(atm.getBalance() == (9000-500));
+		atm.close();
+	}
+	
+	@Test 
+	public  void withdrawBadPin() {
+		Bank testBank = new Bank();
+		testBank.createAccount("5568", "1325", 50);
+		testBank.createAccount("1333", "4444", 20);
+		testBank.createAccount("1324", "1111", 9000);
+		testBank.createAccount("8895", "1956", 200);
+		testBank.createAccount("0001", "7779", 0);
+		ATM atm = new ATM(testBank); //start ATM with bank filled with accounts.
+		assertTrue(atm.cardInserted("8895"));
+		atm.checkPin("2222");
+		atm.withdraw(100);
+		assertFalse(atm.getBalance() == (200-100));
+		atm.checkPin("1956");
+		assertTrue(atm.getBalance() == 200);
+		atm.close();
+	}
+	
+	@Test 
+	public  void atmDeposit() {
+		Bank testBank = new Bank();
+		testBank.createAccount("5568", "1325", 50);
+		testBank.createAccount("1333", "4444", 20);
+		testBank.createAccount("1324", "1111", 9000);
+		testBank.createAccount("8895", "1956", 200);
+		testBank.createAccount("0001", "7779", 0);
+		ATM atm = new ATM(testBank); //start ATM with bank filled with accounts.
+		assertTrue(atm.cardInserted("8895"));
+		assertTrue(atm.deposit(15) == -1);
+		atm.checkPin("1956");
+		atm.deposit(453);
+		assertTrue(atm.getBalance() == 653);
+		atm.close();
+	}
+	@Test
+	public  void checkClosed() {
+		Bank testBank = new Bank();
+		testBank.createAccount("5568", "1325", 50);
+		testBank.createAccount("1333", "4444", 20);
+		testBank.createAccount("1324", "1111", 9000);
+		testBank.createAccount("8895", "1956", 200);
+		testBank.createAccount("0001", "7779", 0);
+		ATM atm = new ATM(testBank); //start ATM with bank filled with accounts.
+		assertTrue(atm.cardInserted("1324"));
+		atm.checkPin("1111");
+		atm.withdraw(500);
+		assertTrue(atm.getBalance() == 8500);
+		atm.deposit(600);
+		assertTrue(atm.getBalance() == 9100);
+		atm.close();
+		assertTrue(atm.getBalance() == 0);
+		assertTrue(atm.withdraw(500) == -1);
+	}
+	// END ATM
+	
+	//SIM TESTS
+	/*
+	public  void testSim() {
+		System.out.println("\n Starting CLI...");
+		//BSim simulator = new Bsim();
 		ATM atm2 = new ATM();
 		atm2.start();
 		Account newAcc = Account();
 	}
-
+	*/
+	// END SIM
 }
